47086eaa9b475d44829d29b6823013fa0cf72c09
matias morant <matias.morant.mm@gmail.com>
2025-01-30 21:36:47 +0000

    v0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,236 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+  await Hive.openBox('entries');
+  await Hive.openBox('settings');
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  final Box settingsBox = Hive.box('settings');
+
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = settingsBox.get('darkMode', defaultValue: false);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: MainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class MainScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MainScreenState createState() => _MainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainScreenState extends State<MainScreen> {
+  final Box box = Hive.box('entries');
+  final Box settingsBox = Hive.box('settings');
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void addEntry() {
+    String name = 'Chemical Substance ${box.length + 1}';
+    box.add(name);
+    setState(() {});
+  }
+
+  void deleteEntry(int index) {
+    box.deleteAt(index);
+    setState(() {});
+  }
+
+  void renameEntry(int index, String newName) {
+    box.putAt(index, newName);
+    setState(() {});
+  }
+
+  void toggleTheme() {
+    bool isDarkMode = settingsBox.get('darkMode', defaultValue: false);
+    settingsBox.put('darkMode', !isDarkMode);
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Entries'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Settings'),
+                content: SwitchListTile(
+                  title: Text('Dark Mode'),
+                  value: settingsBox.get('darkMode', defaultValue: false),
+                  onChanged: (value) {
+                    toggleTheme();
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: ListView.builder(
+        itemCount: box.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(box.getAt(index)),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => DetailScreen(entry: box.getAt(index)),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    TextEditingController controller = TextEditingController(text: box.getAt(index));
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        title: Text('Rename Entry'),
+                        content: TextField(
+                          controller: controller,
+                          decoration: InputDecoration(hintText: 'Enter new name'),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              renameEntry(index, controller.text);
+                              Navigator.pop(context);
+                            },
+                            child: Text('Save'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => deleteEntry(index),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: addEntry,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final String entry;
+  DetailScreen({required this.entry});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(entry)),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => GraphScreen(title: 'Slider')),
+            ),
+            child: Text('Slider'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => GraphScreen(title: 'Kinematics')),
+            ),
+            child: Text('Kinematics'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GraphScreen extends StatefulWidget {
+  final String title;
+  GraphScreen({required this.title});
+
+  @override
+  _GraphScreenState createState() => _GraphScreenState();
+}
+
+class _GraphScreenState extends State<GraphScreen> {
+  List<FlSpot> points = [FlSpot(1, 1), FlSpot(2, 2), FlSpot(3, 3)];
+
+  void _updatePoint(int index, FlSpot newSpot) {
+    setState(() {
+      points[index] = newSpot;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          RenderBox box = context.findRenderObject() as RenderBox;
+          Offset localPosition = box.globalToLocal(details.globalPosition);
+          double x = (localPosition.dx / box.size.width) * 10;
+          double y = (1 - localPosition.dy / box.size.height) * 10;
+          
+          for (int i = 0; i < points.length; i++) {
+            if ((points[i].x - x).abs() < 0.5 && (points[i].y - y).abs() < 0.5) {
+              _updatePoint(i, FlSpot(x, y));
+              break;
+            }
+          }
+        },
+        child: Center(
+          child: Container(
+            height: 300,
+            child: LineChart(
+              LineChartData(
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: points,
+                    isCurved: true,
+                    barWidth: 4,
+                    color: Colors.blue,
+                    belowBarData: BarAreaData(show: false),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
