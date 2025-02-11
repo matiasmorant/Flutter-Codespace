5e183216a1f79b87cd5bf1a52798355c61dfab4d
matias morant <matias.morant.mm@gmail.com>
2025-02-11 16:07:55 +0000

    v0.2.1

--- lib/main.dart
+++ lib/main.dart
@@ -196,37 +196,90 @@ class _GraphScreenState extends State<GraphScreen> {
 
   @override
   Widget build(BuildContext context) {
+    int draggedindex=-1;
+    Offset? dragstart;
+    double minX = points.reduce((current, next) => next.x < current.x ? next : current).x;
+    double maxX = points.reduce((current, next) => next.x > current.x ? next : current).x;
+    double minY = points.reduce((current, next) => next.y < current.y ? next : current).y;
+    double maxY = points.reduce((current, next) => next.y > current.y ? next : current).y;
+
+    LineChartData linechartdata = LineChartData(
+      minX: minX,
+      maxX: maxX,
+      minY: minY,
+      maxY: maxY,
+      lineBarsData: [
+        LineChartBarData(
+          spots: points,
+          isCurved: true,
+          barWidth: 4,
+          color: Colors.blue,
+          belowBarData: BarAreaData(show: false),
+        ),
+      ],
+      lineTouchData: LineTouchData(
+        touchCallback: (FlTouchEvent event, LineTouchResponse? response) {
+          // ScaffoldMessenger.of(context).showSnackBar(
+          //     SnackBar(content: Text('Touched at x: ${event.localPosition?.dx}, y: ${event.localPosition?.dy}')),
+          // );
+          if (event is FlPanEndEvent) {
+            draggedindex=-1;
+          }
+          if (response != null && response.lineBarSpots != null && draggedindex<0) {
+            final spot = response.lineBarSpots!.first;
+            draggedindex = spot.spotIndex;
+            dragstart=event.localPosition;
+            print('selected: ${spot.x},${spot.y} (${dragstart?.dx},${dragstart?.dy})');
+          }
+        },
+        handleBuiltInTouches: true,
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: GestureDetector(
         onPanUpdate: (details) {
+          // double minX = linechartdata.minX;
+          // double maxX = linechartdata.maxX;
+          // double minY = linechartdata.minY;
+          // double maxY = linechartdata.maxY;
+          // FlChartPainter painter = LineChartPainter(lineChartData, showingTooltipSpots: []);
+          // FlSpot bottomLeft = FlSpot(lineChartData.minX!, lineChartData.minY!);
+          // FlSpot topRight = FlSpot(lineChartData.maxX!, lineChartData.maxY!);
+
+          // Offset bottomLeftPx = painter.getPixelForValue(bottomLeft);
+          // Offset topRightPx = painter.getPixelForValue(topRight);
+
           RenderBox box = context.findRenderObject() as RenderBox;
           Offset localPosition = box.globalToLocal(details.globalPosition);
-          double x = (localPosition.dx / box.size.width) * 10;
-          double y = (1 - localPosition.dy / box.size.height) * 10;
-          
-          for (int i = 0; i < points.length; i++) {
-            if ((points[i].x - x).abs() < 0.5 && (points[i].y - y).abs() < 0.5) {
-              _updatePoint(i, FlSpot(x, y));
-              break;
-            }
+          if (draggedindex>=0){
+            print('* ${(linechartdata.maxX-linechartdata.minX)/box.size.width},${(linechartdata.maxY-linechartdata.minY)/box.size.height} (${linechartdata.maxX} ${box.size.height})');
+            double x=points[draggedindex].x+(localPosition.dx-dragstart!.dx)*(linechartdata.maxX-linechartdata.minX)/box.size.width;
+            double y=points[draggedindex].y+(localPosition.dy-dragstart!.dy)*(linechartdata.maxY-linechartdata.minY)/box.size.height;
+            
+            print('${points[draggedindex].x},${points[draggedindex].y} -> $x,$y');
+            print('drag: ${localPosition?.dx},${localPosition?.dy} - ${dragstart!.dx},${dragstart!.dy} = ${localPosition.dx-dragstart!.dx},${localPosition.dy-dragstart!.dy}');
+
+            _updatePoint(draggedindex, FlSpot(x,y));
+            dragstart=localPosition;
           }
+
+          // double x = (localPosition.dx / box.size.width) * 10;
+          // double y = (1 - localPosition.dy / box.size.height) * 10;
+          
+          // for (int i = 0; i < points.length; i++) {
+          //   if ((points[i].x - x).abs() < 0.5 && (points[i].y - y).abs() < 0.5) {
+          //     _updatePoint(i, FlSpot(x, y));
+          //     break;
+          //   }
+          // }
         },
         child: Center(
           child: Container(
             height: 300,
             child: LineChart(
-              LineChartData(
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: points,
-                    isCurved: true,
-                    barWidth: 4,
-                    color: Colors.blue,
-                    belowBarData: BarAreaData(show: false),
-                  ),
-                ],
-              ),
+              linechartdata,
             ),
           ),
         ),
